3673272105d40809b0996f318c64e926c8d60575
Elliot Blanchard <public@elliotblanchard.com>
2020-04-18 16:55:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,15 @@
 class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5], # Middle row
+    [6,7,8], # Bottom row
+    [0,3,6], # Top column
+    [1,4,7], # Middle column
+    [2,5,8], # Bottom column
+    [0,4,8], # Diag A
+    [6,4,2], # Diag B
+  ]
+    
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
